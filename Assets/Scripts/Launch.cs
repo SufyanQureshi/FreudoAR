b73bb6fa023832b961e50a84b688822f2233d414
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-07-02 13:30:32 +0500

    Launch with Smoke working

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    public Animator animFly;
+
+    void Start()
+    {
+        animFly = gameObject.GetComponent<Animator>();
+    }
+    
+    public void PlayAnim()
+    {
+        animFly.SetTrigger("Active");
+    }
+
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public GameObject BlackSmoke;
+
+   
+}
