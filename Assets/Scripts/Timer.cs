67043615f57742222115d0d4aac2514536a71eea
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-07-05 12:44:58 +0500

    Land on moon

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject smoke;
+
+    public void WaitForSmoke()
+    {
+        StartCoroutine(WaitAndDo());
+    }
+
+    IEnumerator WaitAndDo()
+        {
+
+        yield return new  WaitForSeconds(7);
+        smoke.SetActive(false);
+        }
+    
+
+
+}
