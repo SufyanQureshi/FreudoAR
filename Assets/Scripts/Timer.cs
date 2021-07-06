eaa0762d5f02c86e84cf1ff897a70a2680115ff4
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-07-06 15:37:38 +0500

    Animal Destination
    Animal is going towards the rocket and sit down in the rocket.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,22 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public GameObject smoke;
+    public Text countDown;
+    
+
+
 
     public void WaitForSmoke()
     {
         StartCoroutine(WaitAndDo());
+
+
     }
 
-    IEnumerator WaitAndDo()
-        {
 
-        yield return new  WaitForSeconds(7);
+    public void TimerCountDown()
+    {
+        StartCoroutine(CountDown());
+    }
+
+
+    IEnumerator WaitAndDo()
+    {
+
+        yield return new WaitForSeconds(7);
         smoke.SetActive(false);
-        }
+
+    }
+
+    IEnumerator CountDown()
+    {
+        countDown.text = "3";
+        yield return new WaitForSeconds(1.0f);
+        countDown.text = "2";
+        yield return new WaitForSeconds(1.0f);
+        countDown.text = "1";
+        yield return new WaitForSeconds(1.0f);
+        countDown.text = "Go!";
+        yield return new WaitForSeconds(1.0f);
+        countDown.text = "";
+
+        WaitForSmoke();
+    }
     
 
 
