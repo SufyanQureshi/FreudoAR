fcba78ef9a1a377b762ad8d013c21ef818c3cb63
Jameel Qureshi <jameelqureshi2013@gmail.com>
2021-08-11 16:58:44 +0500

    Audio Working

---
+++ Assets/Scripts/ImageTrackableEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+using UnityEngine.Networking;
+using static Vuforia.TrackableBehaviour;
+
+public class ImageTrackableEventHandler : MonoBehaviour
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            
+            mTrackableBehaviour.RegisterOnTrackableStatusChanged(OnTrackableStateChanged);
+        }
+    }
+
+   
+    public void OnTrackableStateChanged(StatusChangeResult obj)
+    {
+        if (obj.NewStatus == Status.DETECTED || obj.NewStatus == Status.TRACKED || obj.NewStatus == Status.EXTENDED_TRACKED)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+
+    
+
+
+}
