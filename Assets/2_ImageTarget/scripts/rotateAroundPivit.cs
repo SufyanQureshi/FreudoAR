8e5d415a0c66580249f3a9ab2ccd35f29b0d31f1
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-09-02 15:29:43 +0500

    2nd Poster

---
+++ Assets/2_ImageTarget/scripts/rotateAroundPivit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateAroundPivit : MonoBehaviour
+{
+    public float rotaionSpeed;
+    public GameObject pivitObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(pivitObject.transform.position,new Vector3(0,1,0),rotaionSpeed*Time.deltaTime);
+    }
+}
